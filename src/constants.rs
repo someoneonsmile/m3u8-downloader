308610042eab32c63a9287b0a3a87a5a4c25f2b2
someoneonsmile <someoneonsmile@gmail.com>
2025-02-24 17:12:17 +0800

    refactor: use const instand of static for constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,7 @@
 use console::Emoji;
 
 /// prefix emoji
-pub(crate) static PREFIX_EMOJIS: [Emoji<'_, '_>; 4] = [
+pub(crate) const PREFIX_EMOJIS: [Emoji<'_, '_>; 4] = [
     Emoji("🛸", ""),
     Emoji("🚀", ""),
     Emoji("🛴", ""),
@@ -9,6 +9,6 @@ pub(crate) static PREFIX_EMOJIS: [Emoji<'_, '_>; 4] = [
 ];
 
 /// 最大同时下载数
-pub(crate) static MAX_PARALLEL_DOWNLOAD: usize = 50;
+pub(crate) const MAX_PARALLEL_DOWNLOAD: usize = 50;
 
-pub(crate) static TS_LIST_PATH: &str = "ts_list.txt";
+pub(crate) const TS_LIST_PATH: &str = "ts_list.txt";
